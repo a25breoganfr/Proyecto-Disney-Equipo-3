1456a1b66ff0a8036e1e46f07cded6146cd02700
a25breoganfr <a25breoganfr@iesantonlosada.gal>
2025-11-12 14:20:57 +0100

    Add files via upload
    model Usuario y eliminar codigo innecesario

--- Disney/src/main/java/model/Usuario.java
+++ Disney/src/main/java/model/Usuario.java
@@ -10,4 +10,29 @@ package model;
  */
 public class Usuario {
     
+    public String nombre;
+    public String contraseña;
+    
+    public Usuario() {
+        this.nombre = "";
+        this.contraseña = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
 }
